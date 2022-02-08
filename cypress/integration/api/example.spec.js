d4513ba787bd8af8b0180627bb5b48df1d1a5628
AekachaiBoonruang <sniperaek@gmail.com>
2022-02-08 15:19:58 +0700

    Update test description

--- cypress/integration/api/example.spec.js
+++ cypress/integration/api/example.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress"/>
 
 describe('Group of test', function () {
-  it('api test', function () {
+  it('API test', function () {
     // see full command at: cypress/support/keywords/api.js
     cy.getTodos()
   })
