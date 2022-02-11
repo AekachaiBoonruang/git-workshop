569c9dc2c55b59ce1da4d905fa00f78f30963870
AekachaiBoonruang <sniperaek@gmail.com>
2022-02-11 14:59:07 +0700

    fix format

--- cypress/integration/frontend/search.spec.js
+++ cypress/integration/frontend/search.spec.js
@@ -1,11 +1,11 @@
 /// <reference types="Cypress"/>
 describe('Google search.', function () {
-    beforeEach(function () {
-      cy.visit('https://www.google.com/')
-    })
-  
-    it('Verify search google by name', function () {
-      cy.get('[name="q"]').type('เอกชัย บุญเรือง {ENTER}')
-      cy.contains('เอกชัย บุญเรือง | Facebook')
+  beforeEach(function () {
+    cy.visit('https://www.google.com/')
+  })
+
+  it('Verify search google by name', function () {
+    cy.get('[name="q"]').type('เอกชัย บุญเรือง {ENTER}')
+    cy.contains('เอกชัย บุญเรือง | Facebook')
   })
 })
