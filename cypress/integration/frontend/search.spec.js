a64c50d34013ccf7bf76678d233fd1004c26d687
AekachaiBoonruang <sniperaek@gmail.com>
2022-02-11 14:26:42 +0700

    Add google search test case

---
+++ cypress/integration/frontend/search.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress"/>
+import * as loginElement from '../../support/pageObject/login'
+describe('Search google', function () {
+  beforeEach(function () {
+    cy.visit('http://google.co.th')
+  })
+
+  it('Search google by name', function () {
+      
+  })
+
+})
