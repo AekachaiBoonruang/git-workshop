ccda66c3b254f07cde2b013409ccf56e3587bedf
Kathawut Mankalad <tam@Atoms-iPad.localdomain>
2022-02-11 14:59:06 +0700

    add search google

--- cypress/integration/frontend/tam.spec.js
+++ cypress/integration/frontend/tam.spec.js
@@ -1,11 +1,11 @@
 /// <reference types="Cypress"/>
-describe('Google search.', function () {
-  beforeEach(function () {
-    cy.visit('https://www.google.com/')
+describe('Google', function () {
+    beforeEach(function () {
+      cy.visit('https://www.google.co.th')
+    })
+  
+    it('Search google', function () {
+        cy.get('.gLFyf').type('20scoops CNX')
+        cy.get('.aajZCb > .lJ9FBc > center > .gNO89b').click()
+    })
   })
-
-  it('Verify search google by name', function () {
-    cy.get('[name="q"]').type('เอกชัย บุญเรือง {ENTER}')
-    cy.contains('เอกชัย บุญเรือง | Facebook')
-  })
-})
