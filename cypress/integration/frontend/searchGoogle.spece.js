d1a30169631b93179d538e9350f00a1da53ced09
poontrkw <poontarika.w@20scoops.net>
2022-02-11 15:09:01 +0700

    Test

--- cypress/integration/frontend/searchGoogle.spece.js
+++ cypress/integration/frontend/searchGoogle.spece.js
@@ -1,8 +1,8 @@
 /// <reference types="Cypress"/>
 describe('Google', function () {
-    beforeEach(function () {
-      cy.visit('http://google.co.th')
-    })
+  beforeEach(function () {
+    cy.visit('http://google.co.th')
+  })
   it('Verify search facebook', function () {
     cy.get('[class="gLFyf gsfi"]').type('Poontarika {ENTER} ')
     cy.contains('Poontarika')
