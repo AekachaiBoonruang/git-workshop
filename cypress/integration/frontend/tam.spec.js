9879705c0dd20477832e5af4f009238372121b31
Kathawut Mankalad <tam@Atoms-iPad.localdomain>
2022-02-11 15:01:37 +0700

    lint check

--- cypress/integration/frontend/tam.spec.js
+++ cypress/integration/frontend/tam.spec.js
@@ -1,11 +1,11 @@
 /// <reference types="Cypress"/>
 describe('Google', function () {
-    beforeEach(function () {
-      cy.visit('https://www.google.co.th')
-    })
-  
-    it('Search google', function () {
-        cy.get('.gLFyf').type('20scoops CNX')
-        cy.get('.aajZCb > .lJ9FBc > center > .gNO89b').click()
-    })
+  beforeEach(function () {
+    cy.visit('https://www.google.co.th')
   })
+
+  it('Search google', function () {
+    cy.get('.gLFyf').type('20scoops CNX')
+    cy.get('.aajZCb > .lJ9FBc > center > .gNO89b').click()
+  })
+})
